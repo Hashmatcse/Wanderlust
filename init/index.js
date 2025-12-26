f1a67988643deed948563636a2d2360a767b0d18
Hashmat <nizamihashmat030@gmail.com>
2025-12-26 22:22:28 +0530

    update MongoDB connection URL and change owner ID in initData

--- init/index.js
+++ init/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb+srv://himanshugurjar1306:GZlaOnOylYEOF3ds@cluster0.3ytsj21.mongodb.net/";
+const MONGO_URL = "mongodb+srv://hashmat26:Hashmat&Atlas@cluster0.mwwidg4.mongodb.net/?appName=Cluster0";
 
 main()
     .then(() => {
@@ -18,7 +18,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({ ...obj, owner: "6892154a2894c22139123422" }));
+    initData.data = initData.data.map((obj) => ({ ...obj, owner: "694537cdc6066879dbdddd3f"}));
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 }
